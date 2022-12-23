33c8fa19345ca79b6332db97e429d3c930892db3
SergeyKaraman <karamans7997@gmail.com>
2022-12-23 13:40:41 +0300

    Урок 6. Одномерные массивы. Продолжение

---
+++ Задача 41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите сколько чисел будет записано: ");
+int Numbers = Convert.ToInt32(Console.ReadLine());
+
+int[] ArrayCreate(int num)
+    {
+        int[] array = new int[num];
+        for (int i = 0; i < array.Length; i++)
+            {   
+                Console.Write($"Введите {i+1} число: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                array[i] = number;
+            }
+        return array;
+    }
+
+int NumberOfPositiveNumbers (int [] array)
+    {
+        int SumPositive = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array [i] > 0) SumPositive++;
+       
+            }
+        return SumPositive;
+    }
+
+int[] NewArray = ArrayCreate(Numbers);
+Console.WriteLine($"Кол-во чисел больше 0 равно -> {NumberOfPositiveNumbers(NewArray)}");
---
+++ Задача 43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Задайте точку b1 для первой прямой: ");
+int  B1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте точку k1 для первой прямой: ");
+int  K1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте точку b2 для второй прямой: ");
+int  B2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте точку k2 для второй прямой: ");
+int K2 = Convert.ToInt32(Console.ReadLine());
+
+double IntersectionOfTwoLines(int b1, int k1, int b2, int k2)
+    {
+        double X1 = k1 - k2;
+        double X2 = b1 - b2; 
+        double x = X2 / -X1;
+        double xAnswer = Math.Round(x, 1);
+        return xAnswer;
+    }
+
+double x = IntersectionOfTwoLines(B1,K1,B2,K2);
+double y1 = K1 * x + B1;
+double straighty1 = Math.Round(y1, 1);
+double y2 = K2 * x + B2;
+double straighty2 = Math.Round(y2, 1); 
+
+Console.WriteLine($"Точки пересечения двух пярмых ({straighty1}; {straighty2}) ");
